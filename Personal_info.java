df89f5d19b5acf658d40af0de3a435c110bd63d4
Augustinek27 <KarlEAugustine@gmail.com>
2024-12-13 17:28:40 -0500

    Add files via upload

---
+++ Personal_info.java
@@ -0,0 +1,23 @@
+package personal_info;
+
+public class Personal_info {
+
+	public static void main(String[] args) {
+		
+			// Declare the variables
+			String firstName = "Jimmy";
+			String lastName = "House";
+			String streetAddress = "148 Planeview Drive";
+			String city = "Punxsutawney";
+			String zipCode = "15767";
+			
+			// Output the results
+			System.out.println(firstName);
+			System.out.println(lastName);
+			System.out.println(streetAddress);
+			System.out.println(city);
+			System.out.println(zipCode);
+			}
+	}
+
+
